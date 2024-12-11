46d4e573aca3fb9be066f78d3bccbab46a4d67a8
BajoueHugo <115192270+BajoueHugo@users.noreply.github.com>
2024-12-11 15:44:19 +0100

    Footer (V3)

--- eye-care-front/src/components/Footer/Footer.tsx
+++ eye-care-front/src/components/Footer/Footer.tsx
@@ -5,63 +5,66 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <div>
-          <h2>Navigation</h2>
-          <p>
-            <Link to="/accueil">Qui sommes-nous ?</Link>
-          </p>
-          <p>
-            <Link to="/articles">Fonctionnement d'un œil ?</Link>
-          </p>
-          <p>
-            <Link to="/tests">Tester votre vue</Link>
-          </p>
-          <p>
-            <Link to="/articles">Les problèmes occulaires</Link>
-          </p>
-          <p>
-            <Link to="/blog">Blog</Link>
-          </p>
-        </div>
-        <div>
-          <h2>Nos Githubs</h2>
-          <p>
-            <a href="https://github.com/Raphi04" target="_blank">
-              Raphael CADETE
-            </a>
-          </p>
-          <p>
-            <a href="https://github.com/Jeremie-PARANT" target="_blank">
-              Jéremie PARANT
-            </a>
-          </p>
-          <p>
-            <a href="https://github.com/BajoueHugo" target="_blank">
-              Hugo BAJOUE
-            </a>
-          </p>
-          <p>
-            <a href="https://github.com/MatthieuTissier" target="_blank">
-              Matthieu TISSIER
-            </a>
-          </p>
-          <p>
-            <a href="https://github.com/ArnoLM7" target="_blank">
-              Arnaud LE MOIL
-            </a>
-          </p>
-          <p id="cr">
-            Copyright ©2024 - Tous droits réservés -
-            <Link to="/mention-légal"> Mentions légales</Link>
-          </p>
-        </div>
-        <div>
-          <h2>Nous Contacter</h2>
-          <p>raphaelcadete04@gmail.com</p>
-          <p>parantjeremie.pro@gmail.com</p>
-          <p>hbajoue@gmail.com</p>
-          <p>matthieutissier77@gmail.com</p>
-          <p>arno.lemoil.pro@gmail.com</p>
+        <hr></hr>
+        <div id="orga">
+          <div>
+            <h2>Navigation</h2>
+            <p>
+              <Link to="/accueil">Qui sommes-nous ?</Link>
+            </p>
+            <p>
+              <Link to="/articles">Fonctionnement d'un œil ?</Link>
+            </p>
+            <p>
+              <Link to="/tests">Tester votre vue</Link>
+            </p>
+            <p>
+              <Link to="/articles">Les problèmes occulaires</Link>
+            </p>
+            <p>
+              <Link to="/blog">Blog</Link>
+            </p>
+          </div>
+          <div>
+            <h2>Nos Githubs</h2>
+            <p>
+              <a href="https://github.com/Raphi04" target="_blank">
+                Raphael CADETE
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/Jeremie-PARANT" target="_blank">
+                Jéremie PARANT
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/BajoueHugo" target="_blank">
+                Hugo BAJOUE
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/MatthieuTissier" target="_blank">
+                Matthieu TISSIER
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/ArnoLM7" target="_blank">
+                Arnaud LE MOIL
+              </a>
+            </p>
+            <p id="cr">
+              Copyright ©2024 - Tous droits réservés -
+              <Link to="/mention-légal"> Mentions légales</Link>
+            </p>
+          </div>
+          <div>
+            <h2>Nous Contacter</h2>
+            <p>raphaelcadete04@gmail.com</p>
+            <p>parantjeremie.pro@gmail.com</p>
+            <p>hbajoue@gmail.com</p>
+            <p>matthieutissier77@gmail.com</p>
+            <p>arno.lemoil.pro@gmail.com</p>
+          </div>
         </div>
       </footer>
     </>
