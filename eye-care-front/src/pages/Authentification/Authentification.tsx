a835a58db27b0f0cc5541e5662156087d72c48c0
Lemoil-Arno <119615707+Lemoil-Arno@users.noreply.github.com>
2024-12-18 15:48:12 +0100

    helvetica medium correction

--- eye-care-front/src/pages/Authentification/Authentification.tsx
+++ eye-care-front/src/pages/Authentification/Authentification.tsx
@@ -2,20 +2,7 @@ import { Outlet } from "react-router-dom";
 import EyeCareLogo from "../../assets/eye-care-logo.svg?react";
 import DayNightMode from "../../components/DayNightMode/DayNightMode";
 
-// import { Navigate } from "react-router-dom";
-interface LoginProps {
-	token: string;
-}
-
-export default function Authentification({ token }: LoginProps) {
-	// if (token !== localStorage.getItem("token")) {
-	//   return (
-	//     <>
-	//       <Navigate to="/" />
-	//     </>
-	//   );
-	// }
-
+export default function Authentification() {
 	return (
 		<>
 			<main className="login">
