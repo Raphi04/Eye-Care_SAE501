a26b74354221921a5f20222cf87894679dc55360
Jeremie-PARANT <parantjeremie@gmail.com>
2024-12-27 18:55:15 +0100

    isLiked

--- eye-care-api/src/Service/PostService.php
+++ eye-care-api/src/Service/PostService.php
@@ -77,58 +77,72 @@ class PostService
         return $post;
     }
 
-    public function postsMapping(array $posts): array
+    public function postsMapping(array $posts, ?User $user): array
     {
         foreach ($posts as $post) {
-            $voteCount = $this->getVoteCount($post);
-
             $responses = [];
             foreach($post->getResponses() as $response)
             {
-                $responseVoteCount = $this->getVoteCount($response);
+                $responseVoteCount = $this->getVoteCount($response, $user);
                 $responses[] = [
                     'id' => $response->getId(),
                     'username' => $response->getUser()->getUsername(),
                     'user_roles' => $response->getUser()->getRoles(),
                     'text' => $response->getText(),
                     'created_at' => $response->getCreatedAt()->setTimezone(new \DateTimeZone('Europe/Paris'))->format('Y-m-d H:i:s'),
-                    'positive_votes' => $responseVoteCount['positive'],
-                    'negative_votes' => $responseVoteCount['negative'],
+                    'like' => $responseVoteCount['like'],
+                    'dislike' => $responseVoteCount['dislike'],
+                    'is_liked' => $responseVoteCount['is_liked'],
+                    'is_disliked' => $responseVoteCount['is_disliked'],
                 ];
             }
 
+            $voteCount = $this->getVoteCount($post, $user);
             $data[] = [
                 'id' => $post->getId(),
                 'username' => $post->getUser()->getUsername(),
                 'user_roles' => $post->getUser()->getRoles(),
                 'text' => $post->getText(),
                 'created_at' => $post->getCreatedAt()->setTimezone(new \DateTimeZone('Europe/Paris'))->format('Y-m-d H:i:s'),
-                'positive_votes' => $voteCount['positive'],
-                'negative_votes' => $voteCount['negative'],
+                'like' => $voteCount['like'],
+                'dislike' => $voteCount['dislike'],
+                'is_liked' => $voteCount['is_liked'],
+                'is_disliked' => $voteCount['is_disliked'],
                 'responses' => $responses
             ];
         }
         return $data;
     }
 
-    public function getVoteCount(Post $post): array
-{
-    $positiveVotes = 0;
-    $negativeVotes = 0;
+    public function getVoteCount(Post $post, ?User $user): array
+    {
+        $positiveVotes = 0;
+        $negativeVotes = 0;
+        $isLiked = false;
+        $isDisliked = false;
 
-    foreach ($post->getVotes() as $vote) {
-        if ($vote->isVoteValue()) {
-            $positiveVotes++;
-        } else {
-            $negativeVotes++;
+        foreach ($post->getVotes() as $vote) {
+            if ($vote->isVoteValue()) {
+                $positiveVotes++;
+            } else {
+                $negativeVotes++;
+            }
+            if ($user && $vote->getUser() === $user){
+                if ($vote->isVoteValue()) {
+                    $isLiked = true;
+                } else {
+                    $isDisliked = true;
+                }
+            }
         }
-    }
 
-    return [
-        'positive' => $positiveVotes,
-        'negative' => $negativeVotes,
-    ];
-}
+        return [
+            'like' => $positiveVotes,
+            'dislike' => $negativeVotes,
+            'is_liked' => $isLiked,
+            'is_disliked' => $isDisliked,
+        ];
+    }
 
     public function persistAndFlush(Post $post): void
     {
