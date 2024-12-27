d14b81ee790f955fff9fe3ef033c96e576e42345
Jeremie-PARANT <parantjeremie@gmail.com>
2024-12-27 15:59:26 +0100

    createVote

---
+++ eye-care-api/src/Service/VoteService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Vote;
+use App\Entity\User;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
+
+class VoteService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function findVoteByPropriety(string $propriety, string $value): ?Vote
+    {
+        $vote = $this->entityManager->getRepository(Vote::class)->findOneBy([$propriety => $value]);
+
+        return $vote;
+    }
+
+    public function findVoteByProprieties(string $propriety1, int $value1, string $propriety2, int $value2): ?Vote
+    {
+        $vote = $this->entityManager->getRepository(Vote::class)->findOneBy([$propriety1 => $value1, $propriety2 => $value2]);
+
+        return $vote;
+    }
+
+    public function createVote(User $user, Post $post, bool $voteValue): Vote
+    {
+        $vote = new Vote();
+        $vote->setUser($user);
+        $vote->setPost($post);
+        $vote->setVoteValue($voteValue);
+
+        return $vote;
+    }
+
+    // public function votesMapping($votes): array
+    // {
+    //     $mappedData = [];
+
+    //     foreach ($votes as $vote) {
+    //         $mappedData[] = [
+    //             'vision_disorder' => $vote->getVisionDisorder()->getDisorderName(),
+    //             'result' => $vote->getResult(),
+    //         ];
+    //     }
+
+    //     return $mappedData;
+    // }
+
+    public function persistAndFlush(Vote $vote): void
+    {
+        $this->entityManager->persist($vote);
+        $this->entityManager->flush();
+    }
+}
