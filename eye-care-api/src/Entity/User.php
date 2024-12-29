f5cef8f80f0ac5e3ed43e345d16e29bf80dbbffe
Jeremie-PARANT <parantjeremie@gmail.com>
2024-12-29 18:26:09 +0100

    username non unique

--- eye-care-api/src/Entity/User.php
+++ eye-care-api/src/Entity/User.php
@@ -34,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255, unique: true)]
+    #[ORM\Column(length: 255)]
     private ?string $username = null;
 
     #[ORM\Column(length: 255, nullable: true)]
