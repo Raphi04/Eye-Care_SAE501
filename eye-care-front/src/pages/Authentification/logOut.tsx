c475b27b3f89cb105ead986ef0ee353129c945db
Raphi04 <raphaelcadete04@gmail.com>
2025-01-03 13:27:41 +0100

    correctif

--- eye-care-front/src/pages/Authentification/logOut.tsx
+++ eye-care-front/src/pages/Authentification/logOut.tsx
@@ -1,49 +1,58 @@
 import { faSpinner } from "@fortawesome/free-solid-svg-icons/faSpinner";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function logOut() {
-	const navigate = useNavigate();
-	const token = localStorage.getItem("token");
-	//URL dynamique de l'API
-	const APIURL = import.meta.env.VITE_API_URL;
+export default function LogOut() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const alreadyDeconnected = useRef(false);
 
-	useEffect(() => {
-		if (token) {
-			const disconnectUser = async () => {
-				const requestOptions = {
-					method: "POST",
-					headers: { "Content-Type": "application/json", "auth-token": token },
-				};
+  //URL dynamique de l'API
+  const APIURL = import.meta.env.VITE_API_URL;
 
-				try {
-					const response = await fetch(`${APIURL}/user/logout`, requestOptions);
+  useEffect(() => {
+    if (token) {
+      const disconnectUser = async () => {
+        alreadyDeconnected.current = true;
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json", "auth-token": token },
+        };
 
-					if (!response.ok) {
-						throw new Error("Erreur HTTP:" + response.status);
-					}
-					localStorage.removeItem("token");
-					localStorage.removeItem("username");
-					navigate("/");
-					//
-				} catch (error: any) {
-					console.log("Erreur lors de l'envoie : " + error);
-					//
-				}
-			};
-			disconnectUser();
-		} else {
-			navigate("/");
-		}
-	}, []);
-	return (
-		<>
-			<div className="mainContent-logOut">
-				<p>
-					<FontAwesomeIcon icon={faSpinner} spin /> Redirection en cours...
-				</p>
-			</div>
-		</>
-	);
+        try {
+          console.log(requestOptions);
+          const response = await fetch(`${APIURL}/user/logout`, requestOptions);
+
+          if (!response.ok) {
+            throw new Error("Erreur HTTP:" + response.status);
+          }
+
+          localStorage.removeItem("token");
+          localStorage.removeItem("username");
+          navigate("/");
+          //
+        } catch (error: any) {
+          console.log("Erreur lors de l'envoie : " + error);
+          //
+        }
+      };
+
+      if (!alreadyDeconnected.current) {
+        disconnectUser();
+      }
+    } else {
+      navigate("/");
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="mainContent-logOut">
+        <p>
+          <FontAwesomeIcon icon={faSpinner} spin /> Redirection en cours...
+        </p>
+      </div>
+    </>
+  );
 }
