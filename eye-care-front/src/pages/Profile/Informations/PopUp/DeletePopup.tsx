57f80304422d88d12a059fcc261e3b16b3d7bb95
Arno_lemoil <arno.lemoil@gmail.com>
2025-01-15 11:11:54 +0100

    correction test.tsx

--- eye-care-front/src/pages/Profile/Informations/PopUp/DeletePopup.tsx
+++ eye-care-front/src/pages/Profile/Informations/PopUp/DeletePopup.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useApiContext } from "../../../../components/ApiProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 interface DeletePopupProps {
 	isOpen: boolean;
@@ -10,6 +12,7 @@ interface DeletePopupProps {
 }
 
 export default function DeletePopup({ isOpen, onClose }: DeletePopupProps) {
+	const [loadingState, setLoadingState] = useState<boolean>(false);
 	const [globalErrors, setGlobalErrors] = useState<string[]>([]);
 	const navigate = useNavigate();
 	const { logoutUser } = useApiContext();
@@ -25,6 +28,7 @@ export default function DeletePopup({ isOpen, onClose }: DeletePopupProps) {
 		setGlobalErrors([]);
 
 		try {
+			setLoadingState(true);
 			if (!token) {
 				setGlobalErrors(["Token d'authentification manquant."]);
 				return;
@@ -49,39 +53,55 @@ export default function DeletePopup({ isOpen, onClose }: DeletePopupProps) {
 				console.error("Erreur inattendue :", error);
 				setGlobalErrors(["Une erreur inattendue s'est produite."]);
 			}
+		} finally {
+			setLoadingState(false);
 		}
 	};
 	return (
-		<div className="popupOverlay">
-			<div className="popupContent">
-				<h1>Supprimer votre compte</h1>
-				<h3>Nous sommes désolés de vous voir partir</h3>
-				{/* Message d'erreur global */}
-				{globalErrors.length > 0 && (
-					<div className="error-container">
-						{globalErrors.map((err, index) => (
-							<p key={index} className="error-message">
-								{err}
-							</p>
-						))}
+		<>
+			<div className="popupOverlay">
+				<div className="popupContent">
+					<h1>Supprimer votre compte</h1>
+					<h3>Nous sommes désolés de vous voir partir</h3>
+					{/* Message d'erreur global */}
+					{globalErrors.length > 0 && (
+						<div className="error-container">
+							{globalErrors.map((err, index) => (
+								<p key={index} className="error-message">
+									{err}
+								</p>
+							))}
+						</div>
+					)}
+					{/* --- Boutons de validation --- */}
+					<div className="popupButtons">
+						<button
+							type="button"
+							className="popupButton none"
+							onClick={onClose}
+						>
+							ANNULER
+						</button>
+						<button
+							type="button"
+							onClick={() => {
+								handleSubmitDelete(stateTokenForDeleteUser);
+							}}
+							className="popupButton danger"
+						>
+							{loadingState ? (
+								<div>
+									<p>
+										<FontAwesomeIcon icon={faSpinner} spin /> Chargement...
+									</p>
+								</div>
+							) : (
+								<p>SUPPRIMER LE COMPTE</p>
+							)}
+						</button>
 					</div>
-				)}
-				{/* --- Boutons de validation --- */}
-				<div className="popupButtons">
-					<button type="button" className="popupButton none" onClick={onClose}>
-						ANNULER
-					</button>
-					<button
-						type="button"
-						onClick={() => {
-							handleSubmitDelete(stateTokenForDeleteUser);
-						}}
-						className="popupButton danger"
-					>
-						SUPPRIMER LE COMPTE
-					</button>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
