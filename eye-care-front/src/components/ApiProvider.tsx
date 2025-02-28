12e03666b875a43fdc5d9302f4391a988bdf3f5c
Raphi04 <raphaelcadete04@gmail.com>
2025-02-28 14:37:25 +0100

    correction bug

--- eye-care-front/src/components/ApiProvider.tsx
+++ eye-care-front/src/components/ApiProvider.tsx
@@ -1,103 +1,95 @@
-import {
-	createContext,
-	ReactNode,
-	useContext,
-	useEffect,
-	useRef,
-	useState,
-} from "react";
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
+
 import { disconnectUser } from "../utils/logout";
 import { useNavigate } from "react-router-dom";
 
 interface ApiContextTyping {
-	connectedUser: any;
-	loadingState: boolean;
-	refreshConnectedUser: () => Promise<void>;
-	logoutUser: () => void;
+  connectedUser: any;
+  loadingState: boolean;
+  refreshConnectedUser: () => Promise<void>;
+  logoutUser: () => void;
 }
 
 const ApiContext = createContext<ApiContextTyping | undefined>(undefined);
 
 export function useApiContext() {
-	const context = useContext(ApiContext);
+  const context = useContext(ApiContext);
 
-	if (!context) {
-		throw new Error(
-			"useApiContext doit être utilisé à l'intérieur d'un ApiProvider"
-		);
-	}
+  if (!context) {
+    throw new Error("useApiContext doit être utilisé à l'intérieur d'un ApiProvider");
+  }
 
-	return context;
+  return context;
 }
 
 export default function ApiProvider({ children }: { children: ReactNode }) {
-	//URL dynamique de l'API
-	const APIURL = import.meta.env.VITE_API_URL;
+  //URL dynamique de l'API
+  const APIURL = import.meta.env.VITE_API_URL;
 
-	const [connectedUser, setConnectedUser] = useState<any>(null);
-	const [loadingState, setLoadingState] = useState<boolean>(false);
-	const token = localStorage.getItem("token");
-	const alreadyGotInformations = useRef(false);
-	const navigate = useNavigate();
+  const [connectedUser, setConnectedUser] = useState<any>(null);
+  const [loadingState, setLoadingState] = useState<boolean>(false);
+  const token = localStorage.getItem("token");
+  const alreadyGotInformations = useRef(false);
+  const navigate = useNavigate();
 
-	const fetchUserData = async () => {
-		if (!token) return;
-		alreadyGotInformations.current = true;
-		setLoadingState(true);
-		try {
-			const response = await fetch(`${APIURL}/user/user_info`, {
-				method: "GET",
-				headers: { "Content-Type": "application/json", "auth-token": token },
-			});
+  async function fetchUserData() {
+    if (!token) return;
+    alreadyGotInformations.current = true;
+    setLoadingState(true);
+    try {
+      const response = await fetch(`${APIURL}/user/user_info`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json", "auth-token": token },
+      });
 
-			if (!response.ok) {
-				throw new Error("Erreur HTTP:" + response.status);
-			}
-			const result = await response.json();
-			setConnectedUser(result);
-		} catch (error) {
-			console.error("Erreur lors de l'envoi : ", error);
-		} finally {
-			setLoadingState(false);
-		}
-	};
+      if (!response.ok) {
+        throw new Error("Erreur HTTP:" + response.status);
+      }
+      const result = await response.json();
+      setConnectedUser(result);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi : ", error);
+    } finally {
+      setLoadingState(false);
+    }
+  }
 
-	// Fonction de rafraîchissement pour mettre à jour les données utilisateur
-	const refreshConnectedUser = async () => {
-		await fetchUserData();
-	};
+  // Fonction de rafraîchissement pour mettre à jour les données utilisateur
+  const refreshConnectedUser = async () => {
+    fetchUserData();
+  };
 
-	const logoutUser = async () => {
-		if (token) {
-			try {
-				await disconnectUser(token);
-				navigate("/");
-			} catch (error) {
-				console.error("Erreur lors de la déconnexion :", error);
-			}
-		}
-		setConnectedUser(null);
-	};
+  const logoutUser = async () => {
+    if (token) {
+      try {
+        await disconnectUser(token);
+        navigate("/");
+      } catch (error) {
+        console.error("Erreur lors de la déconnexion :", error);
+      }
+    }
+    setConnectedUser(null);
+  };
 
-	useEffect(() => {
-		if (token && !alreadyGotInformations.current) {
-			alreadyGotInformations.current = true;
-			fetchUserData();
-		}
-	}, [token]);
+  useEffect(() => {
+    if (token && !alreadyGotInformations.current) {
+      alreadyGotInformations.current = true;
+      fetchUserData();
+    }
+  }, [token]);
 
-	return (
-		<>
-			<ApiContext.Provider
-				value={{
-					connectedUser,
-					loadingState,
-					refreshConnectedUser,
-					logoutUser,
-				}}
-			>
-				{children}
-			</ApiContext.Provider>
-		</>
-	);
+  return (
+    <>
+      <ApiContext.Provider
+        value={{
+          connectedUser,
+          loadingState,
+          refreshConnectedUser,
+          logoutUser,
+        }}
+      >
+        {children}
+      </ApiContext.Provider>
+    </>
+  );
 }
