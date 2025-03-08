f5fb750fdb46881a5ae7018f3d340cc617bcb48e
Arno_lemoil <arno.lemoil@gmail.com>
2025-03-08 12:46:02 +0100

    composant pour l'input file "certificat"

---
+++ eye-care-front/src/components/Authentification/Fields/FieldCertificate.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+
+interface FieldCertificateProps {
+	name: string;
+	type: string;
+	onFileSelect: (file: File | null) => void;
+}
+
+export default function FieldCertificate({
+	name,
+	type,
+	onFileSelect,
+}: FieldCertificateProps) {
+	const [fileName, setFileName] = useState<string>("");
+
+	function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+		const file = event.target.files?.[0] || null;
+		setFileName(file ? file.name : "");
+		onFileSelect(file);
+	}
+
+	return (
+		<div className="containerFileAndImg">
+			<FontAwesomeIcon className="fileImg" icon={faFile} />
+			<div className="containerFieldFile">
+				<p>Déposer un certificat</p>
+				<div className="containerFile">
+					<div className="customFileButton">
+						<input
+							id="fileInput"
+							name={name}
+							type={type}
+							className="fileInput"
+							onChange={handleFileChange}
+						/>
+						<label htmlFor="fileInput">Parcourir</label>
+					</div>
+					{fileName && <p className="fileText-certificate">{fileName}</p>}
+				</div>
+			</div>
+		</div>
+	);
+}
