cac0f4c68f01f815fa8fd86afc66c2aff8478594
Arno_lemoil <arno.lemoil@gmail.com>
2025-09-30 16:57:06 +0200

    illustration articles

--- eye-care-front/src/pages/Articles/Astigmatisme.tsx
+++ eye-care-front/src/pages/Articles/Astigmatisme.tsx
@@ -1,150 +1,157 @@
 import { Link } from "react-router-dom";
 
 import Glasses from "../../assets/glasses.svg?react";
+import astigmatisme from "../../assets/DiseasesImages/astigmatisme.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import CommentsSection from "../../components/CommentsSection/CommentsSection";
 
 export default function Astigmatisme() {
-  return (
-    <>
-      <main>
-        <section className="presentationMaladie">
-          <article className="textContainer">
-            <h2>L'ASTIGMATISME</h2>
-            <p>
-              L’astigmatisme est un trouble visuel qui se caractérise par une
-              vision floue ou déformée, quelle que soit la distance des objets
-              observés. Ce défaut de réfraction peut entraîner une perception
-              altérée des formes et des contours, rendant les lignes droites ou
-              les détails difficiles à discerner. Par exemple, les lettres sur
-              une page ou les panneaux de signalisation peuvent sembler
-              dédoublés ou brouillés. Bien que ce trouble ne soit pas une
-              maladie grave, il peut causer de la fatigue oculaire, des maux de
-              tête et une gêne dans la vie quotidienne, surtout lorsqu’il n’est
-              pas corrigé.
-            </p>
-          </article>
-        </section>
+	return (
+		<>
+			<main>
+				<section className="presentationMaladie">
+					<article className="textContainer">
+						<h2>L'ASTIGMATISME</h2>
+						<div className="textImageContainer">
+							<p>
+								L’astigmatisme est un trouble visuel qui se caractérise par une
+								vision floue ou déformée, quelle que soit la distance des objets
+								observés. Ce défaut de réfraction peut entraîner une perception
+								altérée des formes et des contours, rendant les lignes droites
+								ou les détails difficiles à discerner. Par exemple, les lettres
+								sur une page ou les panneaux de signalisation peuvent sembler
+								dédoublés ou brouillés. Bien que ce trouble ne soit pas une
+								maladie grave, il peut causer de la fatigue oculaire, des maux
+								de tête et une gêne dans la vie quotidienne, surtout lorsqu’il
+								n’est pas corrigé.
+							</p>
+							<img
+								className="imageArticle-astigmatisme"
+								src={astigmatisme}
+							></img>
+						</div>
+					</article>
+				</section>
 
-        <section className="quelquesChiffres">
-          <h2>INFORMATIONS CLÉS</h2>
-          <article className="chiffresContainer">
-            <div className="chiffres">
-              <h3>≃ 30 %</h3>
-              <p>
-                Environ 30 % de la population mondiale présente une forme
-                d'astigmatisme, qui peut varier en termes de sévérité, de léger
-                à sévère.
-              </p>
-            </div>
-            <div className="chiffres">
-              <h3>80 %</h3>
-              <p>
-                Dans 80 % des cas, l'astigmatisme est causé par une anomalie de
-                la cornée, mais il peut également être dû à une irrégularité du
-                cristallin.
-              </p>
-            </div>
-          </article>
-          <article className="chiffresContainer">
-            <div className="chiffres">
-              <h3>≃ 30 - 40 %</h3>
-              <p>
-                Environ 30 à 40 % des personnes souffrant d'astigmatisme ne sont
-                pas conscientes de leur condition, car les symptômes peuvent
-                être légers ou s'aggraver progressivement.
-              </p>
-            </div>
-            <div className="chiffres">
-              <h3>Impact sur la vision</h3>
-              <p>
-                L'astigmatisme provoque une vision floue ou déformée à toutes
-                les distances, rendant difficile la lecture, la conduite, ou
-                même la reconnaissance des visages.
-              </p>
-            </div>
-          </article>
-        </section>
+				<section className="quelquesChiffres">
+					<h2>INFORMATIONS CLÉS</h2>
+					<article className="chiffresContainer">
+						<div className="chiffres">
+							<h3>≃ 30 %</h3>
+							<p>
+								Environ 30 % de la population mondiale présente une forme
+								d'astigmatisme, qui peut varier en termes de sévérité, de léger
+								à sévère.
+							</p>
+						</div>
+						<div className="chiffres">
+							<h3>80 %</h3>
+							<p>
+								Dans 80 % des cas, l'astigmatisme est causé par une anomalie de
+								la cornée, mais il peut également être dû à une irrégularité du
+								cristallin.
+							</p>
+						</div>
+					</article>
+					<article className="chiffresContainer">
+						<div className="chiffres">
+							<h3>≃ 30 - 40 %</h3>
+							<p>
+								Environ 30 à 40 % des personnes souffrant d'astigmatisme ne sont
+								pas conscientes de leur condition, car les symptômes peuvent
+								être légers ou s'aggraver progressivement.
+							</p>
+						</div>
+						<div className="chiffres">
+							<h3>Impact sur la vision</h3>
+							<p>
+								L'astigmatisme provoque une vision floue ou déformée à toutes
+								les distances, rendant difficile la lecture, la conduite, ou
+								même la reconnaissance des visages.
+							</p>
+						</div>
+					</article>
+				</section>
 
-        <section className="causes">
-          <article className="textContainer">
-            <h2>LES CAUSES</h2>
-            <p>
-              L’astigmatisme est principalement causé par une courbure
-              irrégulière de la cornée, qui au lieu d’être parfaitement ronde,
-              adopte une forme plus ovale, semblable à celle d’un ballon de
-              rugby. Cela provoque une focalisation incorrecte des rayons
-              lumineux sur la rétine. Cette anomalie est souvent héréditaire et
-              présente dès la naissance. D’autres facteurs peuvent également
-              être à l’origine de l’astigmatisme, comme des traumatismes
-              oculaires, des infections, ou encore des maladies comme le
-              kératocône, où la cornée s’amincit progressivement. Parfois, il
-              peut aussi être une conséquence de chirurgies oculaires ou de
-              cicatrices sur la cornée.
-            </p>
-          </article>
-          <article className="glasses">
-            <Glasses />
-          </article>
-        </section>
+				<section className="causes">
+					<article className="textContainer">
+						<h2>LES CAUSES</h2>
+						<p>
+							L’astigmatisme est principalement causé par une courbure
+							irrégulière de la cornée, qui au lieu d’être parfaitement ronde,
+							adopte une forme plus ovale, semblable à celle d’un ballon de
+							rugby. Cela provoque une focalisation incorrecte des rayons
+							lumineux sur la rétine. Cette anomalie est souvent héréditaire et
+							présente dès la naissance. D’autres facteurs peuvent également
+							être à l’origine de l’astigmatisme, comme des traumatismes
+							oculaires, des infections, ou encore des maladies comme le
+							kératocône, où la cornée s’amincit progressivement. Parfois, il
+							peut aussi être une conséquence de chirurgies oculaires ou de
+							cicatrices sur la cornée.
+						</p>
+					</article>
+					<article className="glasses">
+						<Glasses />
+					</article>
+				</section>
 
-        <section className="traitements">
-          <h2>LES TRAITEMENTS</h2>
-          <p>
-            Pour corriger l’astigmatisme, plusieurs solutions existent en
-            fonction de son degré de gravité. Les lunettes avec des verres
-            cylindriques sont une méthode simple et efficace pour compenser
-            l’irrégularité de la cornée. Les lentilles de contact, en
-            particulier les lentilles toriques, offrent une correction plus
-            précise, notamment pour les cas modérés à sévères. Pour une solution
-            permanente, la chirurgie réfractive comme le LASIK ou la PKR peut
-            remodeler la cornée pour restaurer une vision nette. Enfin, dans les
-            cas plus graves ou liés à des affections comme le kératocône, des
-            traitements spécifiques comme les implants cornéens ou la greffe de
-            cornée peuvent être envisagés. Un suivi régulier auprès d’un
-            ophtalmologue est essentiel pour surveiller l’évolution du trouble
-            et adapter les corrections si nécessaire.
-          </p>
+				<section className="traitements">
+					<h2>LES TRAITEMENTS</h2>
+					<p>
+						Pour corriger l’astigmatisme, plusieurs solutions existent en
+						fonction de son degré de gravité. Les lunettes avec des verres
+						cylindriques sont une méthode simple et efficace pour compenser
+						l’irrégularité de la cornée. Les lentilles de contact, en
+						particulier les lentilles toriques, offrent une correction plus
+						précise, notamment pour les cas modérés à sévères. Pour une solution
+						permanente, la chirurgie réfractive comme le LASIK ou la PKR peut
+						remodeler la cornée pour restaurer une vision nette. Enfin, dans les
+						cas plus graves ou liés à des affections comme le kératocône, des
+						traitements spécifiques comme les implants cornéens ou la greffe de
+						cornée peuvent être envisagés. Un suivi régulier auprès d’un
+						ophtalmologue est essentiel pour surveiller l’évolution du trouble
+						et adapter les corrections si nécessaire.
+					</p>
 
-          <article className="articlesContainer">
-            <Link to="/articles/dmla" className="articleLink">
-              <div>
-                <h3>LA DMLA</h3>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </div>
-              <p>
-                C'est une affection oculaire chronique qui affecte la macula, la
-                partie centrale de la rétine....
-              </p>
-            </Link>
+					<article className="articlesContainer">
+						<Link to="/articles/dmla" className="articleLink">
+							<div>
+								<h3>LA DMLA</h3>
+								<FontAwesomeIcon icon={faArrowRight} />
+							</div>
+							<p>
+								C'est une affection oculaire chronique qui affecte la macula, la
+								partie centrale de la rétine....
+							</p>
+						</Link>
 
-            <Link to="/articles/daltonisme" className="articleLink">
-              <div>
-                <h3>LE DALTONISME</h3>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </div>
-              <p>
-                C'est un trouble de la vision des couleurs qui empêche certaines
-                personnes....
-              </p>
-            </Link>
+						<Link to="/articles/daltonisme" className="articleLink">
+							<div>
+								<h3>LE DALTONISME</h3>
+								<FontAwesomeIcon icon={faArrowRight} />
+							</div>
+							<p>
+								C'est un trouble de la vision des couleurs qui empêche certaines
+								personnes....
+							</p>
+						</Link>
 
-            <Link to="/articles/presbytie" className="articleLink">
-              <div>
-                <h3>LA PRESBYTIE</h3>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </div>
-              <p>
-                C'est un trouble visuel lié au vieillissement naturel de l'œil,
-                qui se traduit....
-              </p>
-            </Link>
-          </article>
-        </section>
-        <CommentsSection subject="astigmatisme" />
-      </main>
-    </>
-  );
+						<Link to="/articles/presbytie" className="articleLink">
+							<div>
+								<h3>LA PRESBYTIE</h3>
+								<FontAwesomeIcon icon={faArrowRight} />
+							</div>
+							<p>
+								C'est un trouble visuel lié au vieillissement naturel de l'œil,
+								qui se traduit....
+							</p>
+						</Link>
+					</article>
+				</section>
+				<CommentsSection subject="astigmatisme" />
+			</main>
+		</>
+	);
 }
--- eye-care-front/src/pages/Profile/Informations/ProfileContent.tsx
+++ eye-care-front/src/pages/Profile/Informations/ProfileContent.tsx
@@ -3,183 +3,183 @@ import ModifyPopup from "./PopUp/ModifyPopup";
 import DeletePopup from "./PopUp/DeletePopup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faAddressCard,
-  faCheck,
-  faEnvelope,
-  faLock,
+	faAddressCard,
+	faCheck,
+	faEnvelope,
+	faLock,
 } from "@fortawesome/free-solid-svg-icons";
 
 type UserData = {
-  email: string;
-  username: string;
-  vision_disorder: { vision_disorder: string }[];
-  vision_disorder_result: {
-    result: number;
-    vision_disorder: string;
-  }[];
+	email: string;
+	username: string;
+	vision_disorder: { vision_disorder: string }[];
+	vision_disorder_result: {
+		result: number;
+		vision_disorder: string;
+	}[];
 };
 
 type ProfileContentProps = {
-  userData: UserData | null;
-  refreshUserData: () => void;
+	userData: UserData | null;
+	refreshUserData: () => void;
 };
 
 export default function ProfileContent({
-  userData,
-  refreshUserData,
+	userData,
+	refreshUserData,
 }: ProfileContentProps) {
-  const [isModifPopupOpen, setIsModifPopupOpen] = useState(false);
-  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
-  const [email, setEmail] = useState(userData?.email);
-  const [username, setUsername] = useState(userData?.username);
-  const [previousPassword, setPreviousPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [verifNewPassword, setVerifNewPassword] = useState("");
+	const [isModifPopupOpen, setIsModifPopupOpen] = useState(false);
+	const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
+	const [email, setEmail] = useState(userData?.email);
+	const [username, setUsername] = useState(userData?.username);
+	const [previousPassword, setPreviousPassword] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [verifNewPassword, setVerifNewPassword] = useState("");
 
-  useEffect(() => {
-    if (isModifPopupOpen && userData) {
-      setEmail(userData.email);
-      setUsername(userData.username);
-    }
-  }, [isModifPopupOpen, userData]);
+	useEffect(() => {
+		if (isModifPopupOpen && userData) {
+			setEmail(userData.email);
+			setUsername(userData.username);
+		}
+	}, [isModifPopupOpen, userData]);
 
-  // Fonctions pour ouvrir/fermer les pop-up
-  const modifyPopUp = () => {
-    setIsModifPopupOpen(!isModifPopupOpen);
-  };
+	// Fonctions pour ouvrir/fermer les pop-up
+	const modifyPopUp = () => {
+		setIsModifPopupOpen(!isModifPopupOpen);
+	};
 
-  const deletePopUp = () => {
-    setIsDeletePopupOpen(!isDeletePopupOpen);
-  };
+	const deletePopUp = () => {
+		setIsDeletePopupOpen(!isDeletePopupOpen);
+	};
 
-  const hasResults = (userData?.vision_disorder_result ?? []).length > 0;
+	const hasResults = (userData?.vision_disorder_result ?? []).length > 0;
 
-  return (
-    <div className="profileContent">
-      <div className="userInformationsContent">
-        <h2 className="titleInformations">VOS INFORMATIONS</h2>
-        <div className="userInformations">
-          <FontAwesomeIcon icon={faAddressCard} />
-          <p>
-            Nom d'utilisateur : <strong>{userData?.username}</strong>
-          </p>
-        </div>
-        <div className="userInformations">
-          <FontAwesomeIcon icon={faEnvelope} />
-          <p>
-            Email : <strong>{userData?.email || "Utilisateur inconnu"}</strong>
-          </p>
-        </div>
-        <div className="userInformations last">
-          <FontAwesomeIcon icon={faLock} />
-          <p>
-            Mot de passe : <strong>*********</strong>
-          </p>
-        </div>
-        <div className="buttonsInformations">
-          <button
-            type="button"
-            className="buttonInformations modify"
-            onClick={modifyPopUp}
-          >
-            <p>MODIFIER</p>
-          </button>
-          <button
-            type="submit"
-            className="buttonInformations danger"
-            onClick={deletePopUp}
-          >
-            <p>SUPPRIMER LE COMPTE</p>
-          </button>
-        </div>
-      </div>
+	return (
+		<div className="profileContent">
+			<div className="userInformationsContent">
+				<h2 className="titleInformations">VOS INFORMATIONS</h2>
+				<div className="userInformations">
+					<FontAwesomeIcon icon={faAddressCard} />
+					<p>
+						Nom d'utilisateur : <strong>{userData?.username}</strong>
+					</p>
+				</div>
+				<div className="userInformations">
+					<FontAwesomeIcon icon={faEnvelope} />
+					<p>
+						Email : <strong>{userData?.email || "Utilisateur inconnu"}</strong>
+					</p>
+				</div>
+				<div className="userInformations last">
+					<FontAwesomeIcon icon={faLock} />
+					<p>
+						Mot de passe : <strong>*********</strong>
+					</p>
+				</div>
+				<div className="buttonsInformations">
+					<button
+						type="button"
+						className="buttonInformations modify"
+						onClick={modifyPopUp}
+					>
+						<p>MODIFIER</p>
+					</button>
+					<button
+						type="submit"
+						className="buttonInformations danger"
+						onClick={deletePopUp}
+					>
+						<p>SUPPRIMER LE COMPTE</p>
+					</button>
+				</div>
+			</div>
 
-      {/* --- Faire/Refaire les tests --- */}
-      <div className="userInformationsContent last">
-        <h2 className="titleInformations">FAIRE/REFAIRE LES TESTS</h2>
-        <div className="userInformations">
-          <div
-            className={`checkInformationsAcuity ${
-              hasResults
-                ? userData?.vision_disorder_result
-                    .filter((disorder) => disorder.vision_disorder === "myopie")
-                    .map(() => "done")
-                    .join(" ")
-                : ""
-            }`}
-          >
-            {hasResults
-              ? userData?.vision_disorder_result.map((disorder, index) =>
-                  disorder.vision_disorder === "myopie" ? (
-                    <FontAwesomeIcon icon={faCheck} key={index} />
-                  ) : null
-                )
-              : null}
-          </div>
-          <p>Faire le test d'acuité visuelle</p>
-        </div>
-        <div className="userInformations">
-          <div
-            className={`checkInformationsAcuity ${
-              hasResults
-                ? userData?.vision_disorder_result
-                    .filter((disorder) => disorder.vision_disorder === "dmla")
-                    .map(() => "done")
-                    .join(" ")
-                : ""
-            }`}
-          >
-            {hasResults
-              ? userData?.vision_disorder_result.map((disorder, index) =>
-                  disorder.vision_disorder === "DMLA" ? (
-                    <FontAwesomeIcon icon={faCheck} key={index} />
-                  ) : null
-                )
-              : null}
-          </div>
-          <p>Faire le test de DMLA</p>
-        </div>
-        <div className="userInformations">
-          <div
-            className={`checkInformationsAcuity ${
-              hasResults
-                ? userData?.vision_disorder_result
-                    .filter(
-                      (disorder) => disorder.vision_disorder === "daltonisme"
-                    )
-                    .map(() => "done")
-                    .join(" ")
-                : ""
-            }`}
-          >
-            {hasResults
-              ? userData?.vision_disorder_result.map((disorder, index) =>
-                  disorder.vision_disorder === "daltonisme" ? (
-                    <FontAwesomeIcon icon={faCheck} key={index} />
-                  ) : null
-                )
-              : null}
-          </div>
-          <p>Faire le test de d'Ishihara</p>
-        </div>
-      </div>
-      <ModifyPopup
-        isOpen={isModifPopupOpen}
-        onClose={modifyPopUp}
-        username={username || ""}
-        setUsername={setUsername}
-        email={email || ""}
-        setEmail={setEmail}
-        previousPassword={previousPassword}
-        setPreviousPassword={setPreviousPassword}
-        newPassword={newPassword}
-        setNewPassword={setNewPassword}
-        verifNewPassword={verifNewPassword}
-        setVerifNewPassword={setVerifNewPassword}
-        refreshUserData={refreshUserData}
-        setIsModifPopupOpen={setIsModifPopupOpen}
-      />
-      <DeletePopup isOpen={isDeletePopupOpen} onClose={deletePopUp} />
-    </div>
-  );
+			{/* --- Faire/Refaire les tests --- */}
+			<div className="userInformationsContent last">
+				<h2 className="titleInformations">FAIRE/REFAIRE LES TESTS</h2>
+				<div className="userInformations">
+					<div
+						className={`checkInformationsAcuity ${
+							hasResults
+								? userData?.vision_disorder_result
+										.filter((disorder) => disorder.vision_disorder === "myopie")
+										.map(() => "done")
+										.join(" ")
+								: ""
+						}`}
+					>
+						{hasResults
+							? userData?.vision_disorder_result.map((disorder, index) =>
+									disorder.vision_disorder === "myopie" ? (
+										<FontAwesomeIcon icon={faCheck} key={index} />
+									) : null
+							  )
+							: null}
+					</div>
+					<p>Faire le test d'acuité visuelle</p>
+				</div>
+				<div className="userInformations">
+					<div
+						className={`checkInformationsAcuity ${
+							hasResults
+								? userData?.vision_disorder_result
+										.filter((disorder) => disorder.vision_disorder === "dmla")
+										.map(() => "done")
+										.join(" ")
+								: ""
+						}`}
+					>
+						{hasResults
+							? userData?.vision_disorder_result.map((disorder, index) =>
+									disorder.vision_disorder === "dmla" ? (
+										<FontAwesomeIcon icon={faCheck} key={index} />
+									) : null
+							  )
+							: null}
+					</div>
+					<p>Faire le test de DMLA</p>
+				</div>
+				<div className="userInformations">
+					<div
+						className={`checkInformationsAcuity ${
+							hasResults
+								? userData?.vision_disorder_result
+										.filter(
+											(disorder) => disorder.vision_disorder === "daltonisme"
+										)
+										.map(() => "done")
+										.join(" ")
+								: ""
+						}`}
+					>
+						{hasResults
+							? userData?.vision_disorder_result.map((disorder, index) =>
+									disorder.vision_disorder === "daltonisme" ? (
+										<FontAwesomeIcon icon={faCheck} key={index} />
+									) : null
+							  )
+							: null}
+					</div>
+					<p>Faire le test de d'Ishihara</p>
+				</div>
+			</div>
+			<ModifyPopup
+				isOpen={isModifPopupOpen}
+				onClose={modifyPopUp}
+				username={username || ""}
+				setUsername={setUsername}
+				email={email || ""}
+				setEmail={setEmail}
+				previousPassword={previousPassword}
+				setPreviousPassword={setPreviousPassword}
+				newPassword={newPassword}
+				setNewPassword={setNewPassword}
+				verifNewPassword={verifNewPassword}
+				setVerifNewPassword={setVerifNewPassword}
+				refreshUserData={refreshUserData}
+				setIsModifPopupOpen={setIsModifPopupOpen}
+			/>
+			<DeletePopup isOpen={isDeletePopupOpen} onClose={deletePopUp} />
+		</div>
+	);
 }
